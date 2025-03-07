1c3bee0dda7cc4ef9534c81f4f35897e06dc2a2d
Brandon Trent <185147333+Btrent7@users.noreply.github.com>
2025-03-07 13:17:25 -0600

    part_Iterations.py

--- part_Iterations.py
+++ part_Iterations.py
@@ -1,4 +1,4 @@
-#Price Totals by Category (Desc)
+# -- Total Price by PN Desc --
 def partDescription_count(partList, gt1_count, gt2_count, gt3_count):
     for desc in partList.keys():
         if "GT1" in desc:
@@ -46,28 +46,51 @@ rascoParts = {'RASCO,#123, GT2': 150.20,
 
 gt1_count, gt2_count, gt3_count = partDescription_count(rascoParts, gt1_count, gt2_count, gt3_count)
 gt1_price, gt2_price, gt3_price = priceDescription_total(rascoParts, gt1_price, gt2_price, gt3_price)
-
+print(f"\nPrice Totals by Item Description:")
 print(f"GT1 Total Count {gt1_count} : ${round(gt1_price, 2)}")
 print(f"GT2 Total Count {gt2_count} : ${round(gt2_price, 2)}")
-print(f"GT3 Total Count {gt3_count} : ${round(gt3_price, 2)}")
+print(f"GT3 Total Count {gt3_count} : ${round(gt3_price, 2)} \n")
 
-#Inventory alert by Part Number
-def inventory_count(part_inventory):
+# -- PN Inventory Report --
+def inventory_count(part_inventory, pn):
+    print(f"\nInventory Report: {pn}")
     for dc, inventory in part_inventory.items():
         if inventory == 0:
-            print(f"Out of Stock!: {dc}, 0")
+            print(f"Out of Stock!:    {dc}, 0")
         elif inventory < 200:
-            print(f"Iventory Low: {dc}, {inventory}")
+            print(f"Iventory Low:     {dc}, {inventory}")
         elif inventory <= 1000:
             print(f"Inventory Stable: {dc}, {inventory}")
         elif inventory > 1000:
-            print(f"Overstocked: {dc}, {inventory}")
+            print(f"Overstocked:      {dc}, {inventory}")
 
 
-part_6990000001 = {'NY': 1500, 'BN': 1250, 'EG': 190, 'DA': 980, 'PO': 654}
-part_6990000002 = {'NY': 150, 'BN': 950, 'EG': 2190, 'DA': 80, 'PO': 773}
-part_6990000003 = {'NY': 500, 'BN': 250, 'EG': 590, 'DA': 0, 'PO': 954}
+pn_6990000001 = {'NY': 1500, 'BN': 1250, 'EG': 190, 'DA': 980, 'PO': 654}
+pn_6990000002 = {'NY': 150, 'BN': 950, 'EG': 2190, 'DA': 80, 'PO': 773}
+pn_6990000003 = {'NY': 500, 'BN': 250, 'EG': 590, 'DA': 0, 'PO': 954}
 
-inventory_count(part_6990000001)
-inventory_count(part_6990000002)
-inventory_count(part_6990000003)
+inventory_count(pn_6990000001, 'RASCO,#123,GT1 6990000001')
+inventory_count(pn_6990000002, 'RASCO,#234,GT2 6990000002')
+inventory_count(pn_6990000003, 'RASCO,#345,GT3 6990000003')
+
+# -- DIM Invetory Report --
+def inventory_count(dim_inventory, dim):
+    print(f"\nInventory Report: {dim}")
+    for pn, inventory in dim_inventory.items():
+        if inventory == 0:
+            print(f"Out of Stock!:    {pn}, 0")
+        elif inventory < 200:
+            print(f"Iventory Low:     {pn}, {inventory}")
+        elif inventory <= 1000:
+            print(f"Inventory Stable: {pn}, {inventory}")
+        elif inventory > 1000:
+            print(f"Overstocked:      {pn}, {inventory}")
+
+
+dim_ny = {'6990000001': 1500, '6990002378': 1250, '6990000201': 190, '6990000076': 980, '6990002301': 654}
+dim_da = {'6990000001': 120, '6990002378': 220, '6990000201': 1190, '6990000076': 2980, '6990002301': 54}
+dim_hn = {'6990000001': 1320, '6990002378': 20, '6990000201': 122, '6990000076': 80, '6990002301': 754}
+
+inventory_count(dim_ny, 'NY')
+inventory_count(dim_da, 'DA')
+inventory_count(dim_hn, 'HN')
